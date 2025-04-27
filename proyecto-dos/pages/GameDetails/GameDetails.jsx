445a59d02281c49f27f32aa9135cc8cb70b7bf4b
juancruzgw <jgeslowski@gmail.com>
2025-04-27 20:59:47 -0300

    Saque la segunda api, ahora consumimos una sola de 30 elementos

--- proyecto-dos/pages/GameDetails/GameDetails.jsx
+++ proyecto-dos/pages/GameDetails/GameDetails.jsx
@@ -12,12 +12,7 @@ const GameDetails = () => {
   const juegos = await fetch(
     "https://680c06432ea307e081d2fe6b.mockapi.io/API/v1/juegos"
   );
-  const juegos2 = await fetch(
-    "https://680c06432ea307e081d2fe6b.mockapi.io/API/v1/juegos2"
-  );
-  const resp1 = await juegos.json();
-  const resp2 = await juegos2.json();
-  const resp = [...resp1, ...resp2];
+  const resp = await juegos.json();
   setGames(resp);
  };
 
